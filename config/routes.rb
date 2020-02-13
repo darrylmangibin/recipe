7751e70a847324e471b9af9a169ca04c219ca3e9
darryl <darrylmangibin@gmail.com>
2020-02-13 16:59:53 +0800

    create routes and test

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   get "pages/home", to: "pages#home"
 
-  get '/recipes', to: 'recipes#index'
-  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  # get '/recipes', to: 'recipes#index'
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
+  # post 'recipes', to: 'recipes#create'
 
   # resources
-  # resources :recipes
+  resources :recipes
 end
