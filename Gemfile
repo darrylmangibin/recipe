0f7225ad14006b42d15d994f1f9b9d6209d29035
darryl <darrylmangibin@gmail.com>
2020-02-13 13:44:39 +0800

    add TDD and index listing

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ end
 
 group :development do
   gem 'sqlite3'
+  
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
@@ -58,6 +59,7 @@ group :development do
 end
 
 group :test do
+  gem 'rails-controller-testing'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -5,8 +5,23 @@ class RecipesTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    @chef = Chef.create!(name: "mangibin", email: "mangibin@example.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
+    @recipe2.save
+  end
+  
   test "should get recipes index" do
-    get recipes_url
+    get recipes_path
     assert_response :success
   end
+  
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+  end
+
 end
